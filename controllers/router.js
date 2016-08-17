155648dcf6e003bcdc79bcb66e4322b2dd9155a5
Matt Bajorek <bajorek.matthew@gmail.com>
2016-08-17 12:28:44 -0400

    Moved authentication to separate file

--- controllers/router.js
+++ controllers/router.js
@@ -6,6 +6,7 @@ var router = express.Router();
 var path = require('path');
 var passport = require('passport');
 var flash = require('connect-flash');
+var isLoggedIn = require('./authentication');
 //var burger = require('../models/burger.js');
 
 // =====================================
@@ -79,16 +80,5 @@ router.get('/logout', function(req, res) {
 	req.logout();
 	res.redirect('/');
 });
-
-// Route middleware to make sure
-function isLoggedIn(req, res, next) {
-
-	// if user is authenticated in the session, carry on
-	if (req.isAuthenticated())
-		return next();
-
-	// if they aren't redirect them to the home page
-	res.redirect('/');
-}
 
 module.exports = router;
