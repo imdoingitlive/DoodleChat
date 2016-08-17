35339af85f8bd2beba0310a3380c0121a3bf327e
Matt Bajorek <bajorek.matthew@gmail.com>
2016-08-17 11:57:41 -0400

    Basic authentication with passport, bcrypt, and mysql queries

--- controllers/router.js
+++ controllers/router.js
@@ -4,10 +4,95 @@ Here is where you create all the functions that will do the routing for your app
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var passport = require('passport');
+var flash = require('connect-flash');
 //var burger = require('../models/burger.js');
 
-router.get('/', function (req, res) {
+// =====================================
+// HOME PAGE (with login links) ========
+// =====================================
+router.get('/', function(req, res) {
+	res.render('index'); // load the index.ejs file
+});
+
+// =====================================
+// LOGIN ===============================
+// =====================================
+// show the login form
+router.get('/login', function(req, res) {
+	// render the page and pass in any flash data if it exists
+	res.render('login', {
+		message: req.flash('loginMessage')
+	});
+});
+
+// process the login form
+router.post('/login', passport.authenticate('local-login', {
+		successRedirect: '/profile', // redirect to the secure profile section
+		failureRedirect: '/login', // redirect back to the signup page if there is an error
+		failureFlash: true // allow flash messages
+	}),
+	function(req, res) {
+		console.log("hello");
+
+		if (req.body.remember) {
+			req.session.cookie.maxAge = 1000 * 60 * 3;
+		} else {
+			req.session.cookie.expires = false;
+		}
+		res.redirect('/');
+	});
+
+// =====================================
+// SIGNUP ==============================
+// =====================================
+// show the signup form
+router.get('/signup', function(req, res) {
+	// render the page and pass in any flash data if it exists
+	res.render('signup', {
+		message: req.flash('signupMessage')
+	});
+});
+
+// process the signup form
+router.post('/signup', passport.authenticate('local-signup', {
+	successRedirect: '/profile', // redirect to the secure profile section
+	failureRedirect: '/signup', // redirect back to the signup page if there is an error
+	failureFlash: true // allow flash messages
+}));
+
+// =====================================
+// PROFILE SECTION =========================
+// =====================================
+// we will want this protected so you have to be logged in to visit
+// we will use route middleware to verify this (the isLoggedIn function)
+router.get('/profile', isLoggedIn, function(req, res) {
+	res.render('profile', {
+		user: req.user // get the user out of session and pass to template
+	});
+});
+
+// =====================================
+// LOGOUT ==============================
+// =====================================
+router.get('/logout', function(req, res) {
+	req.logout();
+	res.redirect('/');
+});
+
+router.get('/sketch', function(req, res) {
 	res.sendFile(path.resolve('public/assets/sketch.html'));
 });
+
+// Route middleware to make sure
+function isLoggedIn(req, res, next) {
+
+	// if user is authenticated in the session, carry on
+	if (req.isAuthenticated())
+		return next();
+
+	// if they aren't redirect them to the home page
+	res.redirect('/');
+}
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,30 +1,49 @@
-/*
-Here is where you set up your server file.
-express middleware.
-*/
-
+// set up ======================================================================
+// get all the tools we need
 var express = require('express');
+var session  = require('express-session');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var morgan = require('morgan');
 var methodOverride = require('method-override');
+var passport = require('passport');
+var flash    = require('connect-flash')
 
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-global.db = require('./models');
+//global.db = require('./models');
+
+// Config Passport
+require('./config/passport')(passport); // pass passport for configuration
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() + '/public'));
 
-app.use(bodyParser.urlencoded({
+// Set up middleware
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.urlencoded({ // body parser for reading body requests
 	extended: false
 }));
-// override with POST having ?_method=DELETE
-app.use(methodOverride('_method'));
+app.use(methodOverride('_method')); // Override with POST having ?_method=DELETE
+
+// Set up view engine
 var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({
 	defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
+
+// Required for passport
+app.use(session({
+	secret: 'supersecretpassword',
+	resave: true,
+	saveUninitialized: true
+ } )); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
 // Routing
 var routes = require('./controllers/router');
@@ -38,13 +57,17 @@ io.on('connection', socketio.connection);
 var PORT = process.env.PORT || 3000;
 
 // sync with sequelize and start listening
-db.sequelize.query('SET FOREIGN_KEY_CHECKS = 0')
-.then(function() {
-    return db.sequelize.sync({
-        force: true
-    })
-}).then(function() {
-    app.listen(PORT, function() {
-        console.log("Server running on port %s", PORT);
-    });
+// db.sequelize.query('SET FOREIGN_KEY_CHECKS = 0')
+// .then(function() {
+//     return db.sequelize.sync({
+//         force: true
+//     })
+// }).then(function() {
+//     app.listen(PORT, function() {
+//         console.log("Server running on port %s", PORT);
+//     });
+// });
+
+app.listen(PORT, function() {
+  console.log("Server running on port %s", PORT);
 });
