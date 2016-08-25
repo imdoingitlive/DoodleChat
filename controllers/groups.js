ea252ce250879af29a0bdf559833a8634d9c469d
Matt Bajorek <bajorek.matthew@gmail.com>
2016-08-25 16:17:22 -0400

    Fixed recent group callback error

--- controllers/groups.js
+++ controllers/groups.js
@@ -12,42 +12,48 @@ module.exports = function(req, res) {
 	// Find most recently created groups
 	models.Group.findAll({
 		limit: 5,
-		order: 'created_at DESC'
+		order: 'id DESC'
 		// Create a loop through function that is called recursively
 	}).then(function loopThrough(recentGroups, counter) {
-			// Set counter
-			if (counter === undefined) counter = 0;
-			if (counter >= recentGroups.length) return;
-			// Get groupname and totalusers
-			var obj = {
-				groupname: recentGroups[counter].dataValues.groupname,
-				totalusers: recentGroups[counter].dataValues.totalusers
-			}
-			// Check if total users is reached
-			if (obj.totalusers === 4) {
-	  		obj.joined = true; // Even though not actually joined, it does not display ability to join
-	  		// Push obj to recent groups
+		// Set counter
+		if (counter === undefined) counter = 0;
+		if (counter >= recentGroups.length) {
+			cb()
+			return
+		}
+		// Get groupname and totalusers
+		var obj = {
+			groupname: recentGroups[counter].dataValues.groupname,
+			totalusers: recentGroups[counter].dataValues.totalusers
+		}
+		// Check if total users is reached
+		if (obj.totalusers === 4) {
+  		obj.joined = true; // Even though not actually joined, it does not display ability to join
+  		// Push obj to recent groups
+    	hbsObject.recentGroups.push(obj);
+    	// Recursion
+    	counter++;
+    	loopThrough(recentGroups, counter)
+  	} else {
+  		// Check if user is in group
+    	recentGroups[counter].getUsers({where : {username: req.user.username}}).then(function(results) {
+    		// If no result, not used yet
+	    	if (results.length === 0)
+	    		obj.joined = false;
+	    	else
+	    		obj.joined = true;
+	    	// Push obj to recent groups
 	    	hbsObject.recentGroups.push(obj);
 	    	// Recursion
 	    	counter++;
 	    	loopThrough(recentGroups, counter)
-	  	} else {
-	  		// Check if user is in group
-	    	recentGroups[counter].getUsers({where : {username: req.user.username}}).then(function(results) {
-	    		// If no result, not used yet
-		    	if (results.length === 0)
-		    		obj.joined = false;
-		    	else
-		    		obj.joined = true;
-		    	// Push obj to recent groups
-		    	hbsObject.recentGroups.push(obj);
-		    	// Recursion
-		    	counter++;
-		    	loopThrough(recentGroups, counter)
-	    	})
-	  	}				
+    	})
+  	}				
 
-	}).then(function() {
+	})
+
+	// Used a callback function instead of then (then was causing problems)
+	function cb() {
 		// Get sequelize user object
 		models.User.findOne({
 			where: {username: req.user.username}
@@ -72,6 +78,6 @@ module.exports = function(req, res) {
 		}).error(function(err) {
 	    console.log(err);
 	  })
-	})
+	}
 		
 }
